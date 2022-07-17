9820d7738f029aee9d5ca249b83caa89fcd45ac7
DavidYauri1 <david.yauri@tecsup.edu.pe>
2022-07-17 16:22:48 -0500

    Create Page

--- resources/views/admin/reservations/index.blade.php
+++ resources/views/admin/reservations/index.blade.php
@@ -7,6 +7,9 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+    <div class="flex justify-end m2 p-2">
+        <a href="{{ route('admin.reservations.create') }}" class="px-4 py-2 bg-indigo-500 hover:bg-indigo-700 rounded-lg text-white" >New Reservation</a>
+    </div>
            
 <div class="overflow-x-auto relative">
     <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
